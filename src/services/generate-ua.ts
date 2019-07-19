266be3383b563537af39e3b5b5900aa2f4fcd4db
Weiyu-Chen <1300546909@qq.com>
2019-07-19 22:12:57 +0800

    perf: ua perf

--- src/services/generate-ua.ts
+++ src/services/generate-ua.ts
@@ -1,11 +1,43 @@
 import UserAgent from 'user-agents'
+import faker from 'faker'
+import { WeigthObj, getRandomItem } from '../utils/common'
+
+const chineseUas: WeigthObj[] = [
+  {
+    weigth: 20,
+    value: `360SE`
+  },
+  {
+    weigth: 10,
+    value: `SE 2.X MetaSr 3.2.124`
+  },
+  {
+    weigth: 10,
+    value: 'QQBrowser/6.5.9225.201'
+  },
+  {
+    weigth: 5,
+    value: `BAIDUBrowser 3.8.2311.100`
+  },
+  {
+    value: `Maxthon/3.0 Safari/533.9`,
+    weigth: 10
+  },
+  {
+    value: `UBrowser/4.0.3214.0 Safari/537.36`,
+    weigth: 10
+  },
+  {
+    value: ``,
+    weigth: 50
+  }
+]
 
 export const generateUserAgent = () => {
-  // const ua = faker.internet.userAgent()
-  const randomNum = Math.random() * 100
   let ua = ''
-  if (randomNum < 15) {
-    ua = new UserAgent()
+  const chineseUa: string = getRandomItem(chineseUas)
+  if (chineseUa) {
+    ua = `${faker.internet.userAgent()} ${chineseUa}`
   } else {
     ua = new UserAgent({
       platform: 'Win32'
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -3,3 +3,21 @@ export const sleep = (duration: number) => {
     setTimeout(resolve, duration)
   })
 }
+
+export interface WeigthObj {
+  weigth: number
+  value: any
+}
+
+export const getRandomItem = (arr: WeigthObj[]): WeigthObj['value'] => {
+  const sumWeight = arr.reduce((prev, cur) => prev + cur.weigth, 0)
+  let randomNum = Math.random() * sumWeight
+  for (const item of arr) {
+    if (randomNum <= item.weigth) {
+      return item.value
+    }
+    randomNum -= item.weigth
+  }
+  console.log('arr', arr)
+  throw new Error('未知错误')
+}
