b99259a0422670d1c3b70c9d270c40f364decf51
Weiyu-Chen <1300546909@qq.com>
2019-07-20 22:32:01 +0800

    feat: add xicidaili

--- src/ip-spider.ts
+++ src/ip-spider.ts
@@ -3,21 +3,25 @@ import puppeteer from 'puppeteer'
 import { ormconfig } from 'config/ormconfig'
 import { getIpFromXila } from './spider-task/spider-ip-xila'
 import { getIpFromKuai } from './spider-task/spider-ip-kuai'
+import { getIpFromXici } from './spider-task/spider-ip-xici'
 
 async function main() {
   const connection = await createConnection(ormconfig)
   const browser = await puppeteer.launch({
-    headless: false,
-    slowMo: 300
-
+    // headless: false,
+    // slowMo: 300
     // devtools: true,
   })
 
   const xilaPage = await browser.newPage()
-
   const kuaiPage = await browser.newPage()
+  const xiciPage = await browser.newPage()
 
-  await Promise.all([getIpFromXila(xilaPage, connection), getIpFromKuai(kuaiPage, connection)])
+  await Promise.all([
+    getIpFromXila(xilaPage, connection),
+    getIpFromKuai(kuaiPage, connection),
+    getIpFromXici(xiciPage, connection)
+  ])
 
   browser.close()
 }
